295c483c04d5898b7ad710fa917ff1f2e49814a1
kucha lana <bloggerlelaki@gmail.com>
2017-01-31 14:24:09 +0800

    Update copyclipboard.js

--- copyclipboard.js
+++ copyclipboard.js
@@ -1,5 +1,4 @@
-<script>
-    function detectIE() {
+function detectIE() {
         var ua = window.navigator.userAgent;
 
         var msie = ua.indexOf('MSIE ');
@@ -91,4 +90,3 @@
         }
 
     }
-</script>
